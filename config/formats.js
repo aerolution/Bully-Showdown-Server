0e8c9831b33034be90247e1552bf4f59990a8dff
Jackinev <39590350+Jackinev@users.noreply.github.com>
2018-11-23 11:37:38 +0100

    Fix

--- config/formats.js
+++ config/formats.js
@@ -1157,27 +1157,7 @@ let Formats = [
 			return Object.assign({}, template, {types: types});
 		},
     },
-	{
-		name: "[Gen 7] Averagemons",
-		desc: `Every Pok&eacute;mon, including formes, has base 100 in every stat.`,
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/forums/threads/3590605/">Averagemons</a>`,
-		],
-
-		mod: 'gen7',
-		ruleset: ['Pokemon', 'Standard', 'Team Preview'],
-		banlist: [
-			'Gengar-Mega', 'Mawile-Mega', 'Medicham-Mega', 'Smeargle',
-			'Arena Trap', 'Huge Power', 'Pure Power', 'Shadow Tag', 'Deep Sea Tooth', 'Eviolite', 'Light Ball', 'Thick Club', 'Baton Pass', 'Chatter',
-		],
-		onModifyTemplate: function (template) {
-			let dex = this && this.deepClone ? this : Dex;
-			let newTemplate = dex.deepClone(template);
-			newTemplate.baseStats = {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100};
-			return newTemplate;
-		},
-	},
-	{
+	/**{
 		name: "[Gen 7] Random Averagemons",
 		desc: `Every Pok&eacute;mon, including formes, has base 100 in every stat.`,
 
@@ -1190,7 +1170,7 @@ let Formats = [
 			newTemplate.baseStats = {hp: 100, atk: 100, def: 100, spa: 100, spd: 100, spe: 100};
 			return newTemplate;
 		},
-	},
+	},**/
 	{
 		name: "[Gen 7] Tier Shift",
 		desc: `Pok&eacute;mon get a +10 boost to each stat per tier below OU they are in.`,

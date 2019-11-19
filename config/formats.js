eaa17837a5c09df3ba63a8dda865f98e421a5ea6
Jackinev <39590350+Jackinev@users.noreply.github.com>
2019-11-19 19:46:08 +0100

    Remove Super Staff Bros

--- config/formats.js
+++ config/formats.js
@@ -679,40 +679,6 @@ let Formats = [
 		ruleset: ['Obtainable', 'Standard GBU'],
 	},
 	{
-		name: "[Gen 7] Super Staff Bros Brawl",
-		desc: "Super Staff Bros returns for another round! Battle with a random team of pokemon created by the sim staff.",
-		threads: [
-			`&bullet; <a href="https://www.smogon.com/articles/super-staff-bros-brawl">Introduction &amp; Roster</a>`,
-		],
-
-		mod: 'ssb',
-		team: 'randomStaffBros',
-		ruleset: ['HP Percentage Mod', 'Cancel Mod', 'Sleep Clause Mod'],
-		onBegin() {
-			this.add('raw|SUPER STAFF BROS <b>BRAWL</b>!!');
-			this.add('message', 'GET READY FOR THE NEXT BATTLE!');
-			if (this.teamGenerator.allXfix) this.add(`c|~HoeenHero|Oops I dropped my bag of xfix sets sorry!`);
-			this.add(`raw|<div class='broadcast-green'><b>Wondering what all these custom moves, abilities, and items do?<br />Check out the <a href="https://www.smogon.com/articles/super-staff-bros-brawl" target="_blank">Super Staff Bros Brawl Guide</a> and find out!</b></div>`);
-		},
-		onSwitchInPriority: 100,
-		onSwitchIn(pokemon) {
-			let name = toID(pokemon.illusion ? pokemon.illusion.name : pokemon.name);
-			if (this.dex.getTemplate(name).exists || name === 'rage') {
-				// Certain pokemon have volatiles named after their speciesid
-				// To prevent overwriting those, and to prevent accidentaly leaking
-				// that a pokemon is on a team through the onStart even triggering
-				// at the start of a match, users with pokemon names will need their
-				// statuse's to end in "user".
-				name = /** @type {ID} */(name + 'user');
-			}
-			// Add the mon's status effect to it as a volatile.
-			let status = this.dex.getEffect(name);
-			if (status && status.exists) {
-				pokemon.addVolatile(name, pokemon);
-			}
-		},
-	},
-	{
 		name: "[Gen 6] Random Battle",
 
 		mod: 'gen6',
